3a1e512b3ea6a2eaf25307264ef5ed34bf145e31
Rik Prohaska <prohaska7@gmail.com>
2017-01-04 15:17:04 -0500

    handle whitespace in dir and file names

--- s3getdir.py
+++ s3getdir.py
@@ -2,14 +2,14 @@
 set -e
 if [ $# != 2 ] ; then exit 1; fi
 bucket=$1
-files=$(s3ls $bucket --prefix=$2)
-for file in $files; do
-    d=$(dirname $file)
-    f=$(basename $file)
-    if [ ! -d $d ] ; then mkdir -p $d; fi
-    if [ ! -f $d/$f ] ; then 
-        pushd $d
-        s3get $bucket $file $f
+s3ls $bucket --prefix="$2" | \
+while read file; do
+    d=$(dirname "$file")
+    f=$(basename "$file")
+    if [ ! -d "$d" ] ; then mkdir -p "$d"; fi
+    if [ ! -f "$d"/"$f" ] ; then 
+        pushd "$d"
+        s3get $bucket "$file" "$f"
         popd
     fi
 done
--- s3putdir.py
+++ s3putdir.py
@@ -2,9 +2,9 @@
 set -e
 if [ $# != 2 ] ; then exit 1; fi
 bucket=$1
-files=$(find $2 -type f -print)
-for f in $files; do
-    if [ -f $f ] ; then
-        s3put $bucket $f $f
+find $2 -type f | \
+while read f; do
+    if [ -f "$f" ] ; then
+	s3put $bucket "$f" "$f"
     fi
 done
