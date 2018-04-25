2fe6863648c9ef70eb67d4e211dd52a53607bfef
rik prohaska <prohaska7@gmail.com>
2018-04-25 12:08:50 -0400

    add verbose option

--- s3getdir.sh
+++ s3getdir.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 set -e
+verbose=0
+while [ $# -gt 0 ] ; do
+    if [ "$1" = "--verbose" ] ; then verbose=1; shift; else break; fi
+done
 if [ $# != 2 ] ; then exit 1; fi
 bucket=$1
 s3ls $bucket --prefix="$2" | \
@@ -8,8 +12,9 @@ while read file; do
     f=$(basename "$file")
     if [ ! -d "$d" ] ; then mkdir -p "$d"; fi
     if [ ! -f "$d"/"$f" ] ; then 
-        pushd "$d"
+        pushd "$d" >/dev/null
+        if [ $verbose -ne 0 ] ; then echo s3get $bucket "$file" "$f"; fi
         s3get $bucket "$file" "$f"
-        popd
+        popd >/dev/null
     fi
 done
--- s3putdir.sh
+++ s3putdir.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 set -e
+verbose=0
+while [ $# -gt 0 ] ; do
+    if [ "$1" = "--verbose" ] ; then verbose=1; shift; else break; fi
+done
 if [ $# != 2 ] ; then exit 1; fi
 bucket=$1
 find -L $2 -type f | \
 while read f; do
     if [ -f "$f" ] ; then
+        if [ $verbose -ne 0 ] ; then echop s3put $bucket "$f" "$f"
 	s3put $bucket "$f" "$f"
     fi
 done
