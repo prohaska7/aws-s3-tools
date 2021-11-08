3c521d453fd3f51690f5a3a4b128b5bc02d6bb5e
Rik Prohaska <prohaska7@gmail.com>
2021-11-08 08:12:51 -0500

    remove obsolete code

--- s3getdir.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-getargs="-r"
-while [ $# -gt 0 ] ; do
-    if [ "$1" = "--verbose" ] ; then getargs="$getargs $1"; shift; else break; fi
-done
-if [ $# != 2 ] ; then exit 1; fi
-s3get $getargs "$1" "$2"
--- s3putdir.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-putargs="-r"
-while [ $# -gt 0 ] ; do
-    if [ "$1" = "--verbose" ] ; then putargs="$putargs $1"; shift; else break; fi
-done
-if [ $# != 2 ] ; then exit 1; fi
-s3put $putargs "$1" "$2"
