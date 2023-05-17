8411d0417c0374d1d453e02d5d9e1a3bf35fb6bd
Rik Prohaska <prohaska7@gmail.com>
2023-05-17 09:59:27 -0400

    Allow local or S3 repositories.

--- s3snap-fixup.py
+++ s3snap-fixup.py
@@ -9,7 +9,7 @@
 # change is not an md5 checksum, so there is no way to validate the data in
 # the destination object.
 
-# s3fixup finds all of the objects in the destination bucket that are missing
+# s3snap-fixup finds all of the objects in the destination bucket that are missing
 # the user-md5 metadata due to the multipart copy operation.  it retrieves the user-md5
 # metadata from the source object and copies it the the destination object.  if
 # the source object does not have the user-md5 metadata, this utility downloads
@@ -28,10 +28,89 @@ import tempfile
 verbose = 0
 
 def usage():
-    print('[--help] [--verbose] [--prefix=the_prefix] [--size=the_size_limit] src_bucket dest_bucket', file=sys.stderr)
+    print('[--help] [--verbose] [--size=THE_SIZE_LIMIT] SRC DEST', file=sys.stderr)
+
+class LocalRepo:
+    def __init__(self, dname):
+        self.dname = dname
+    def __str__(self):
+        if self.dname.startswith('/'):
+            return self.dname
+        else:
+            return os.getcwd() + '/' + self.dname
+    def get_type(self):
+        return 'F'
+    def get_size(self, key):
+        fkey = self.get_full_key(key)
+        return os.stat(fkey).st_size
+    def get_md5(self, key):
+        fkey = self.get_full_key(key)
+        return compute_md5(fkey)
+    def get_full_key(self, key):
+        return self.dname + '/' + key
+    def get_full_name(self, key):
+        fkey = self.get_full_key(key)
+        if fkey.startswith('/'):
+            return fkey
+        else:
+            return os.getcwd() + '/' + fkey
+
+class S3Repo:
+    def __init__(self, bucket_name, prefix):
+        self.bucket_name = bucket_name
+        self.prefix = prefix
+        self.s3 = boto3.resource('s3')
+        self.bucket = self.s3.Bucket(bucket_name)
+        self.tempname = None
+    def __del__(self):
+        if self.tempname is not None:
+            os.unlink(self.tempname)
+            self.tempname = None
+    def __str__(self):
+        s3name = 's3://' + self.bucket_name
+        if self.prefix != '':
+            s3name += '/' + self.prefix
+        return s3name
+    def get_type(self):
+        return 'S'
+    def get_keys(self):
+        keys = []
+        filter_prefix = self.prefix
+        if filter_prefix !=  '':
+            filter_prefix += '/'
+        for k in self.bucket.objects.filter(Prefix=filter_prefix):
+            key = k.key
+            if self.prefix != '' and key.startswith(self.prefix + '/'):
+                key = key[len(self.prefix)+1:]
+            keys.append(key)
+        return keys
+    def get_full_key(self, key):
+        if self.prefix != '':
+            return self.prefix + '/' + key
+        else:
+            return key
+    def get_full_name(self, key):
+        return 's3://' + self.bucket_name + '/' + self.get_full_key(key)
+    def get_size(self, key):
+        obj = self.bucket.Object(self.get_full_key(key))
+        return obj.content_length
+    def get_md5(self, key):
+        obj = self.bucket.Object(self.get_full_key(key))
+        md5 = None
+        if 'user-md5' in obj.metadata:
+            md5 = obj.metadata['user-md5']
+        else:
+            md5 = None
+        return md5
+    def set_md5(self, key, md5):
+        full_key = self.get_full_key(key)
+        global verbose
+        if verbose: print('set_md5', full_key, md5)
+        obj = self.bucket.Object(full_key)
+        obj.metadata.update({'user-md5': md5})
+        obj.copy_from(CopySource={'Bucket': self.bucket_name, 'Key': full_key}, Metadata=obj.metadata, MetadataDirective='REPLACE')
 
 def main():
-    prefix = ''
     size_limit = None
     buckets = []
     for arg in sys.argv[1:]:
@@ -39,18 +118,15 @@ def main():
             if arg == "-h" or arg == "-?" or arg == "--help":
                 usage()
                 return 1
-            match = re.match('^--prefix=(.*)', arg)
-            if match:
-                prefix = match.group(1)
+            if arg == '-v' or arg == '--verbose':
+                global verbose
+                verbose = 1
                 continue
             match = re.match('^--size=(.*)', arg)
             if match:
                 size_limit = int(match.group(1))
             continue # eat unknown args
-        match = re.match('^s3://(.*)', arg)
-        if match:
-            arg = match.group(1)
-        buckets.append(arg)
+        buckets.append(get_repo(arg))
 
     if len(buckets) != 2:
         print(len(buckets))
@@ -58,9 +134,7 @@ def main():
         return 1
 
     try:
-        s3 = boto3.resource('s3')
-        s3client = boto3.client('s3')
-        fixup_bucket(s3, s3client, buckets[0], buckets[1], prefix, size_limit)
+        fixup_bucket(buckets[0], buckets[1], size_limit)
     except:
         e = sys.exc_info()
         print(e, file=sys.stderr)
@@ -69,69 +143,60 @@ def main():
 
     return 0
 
-def fixup_bucket(s3, s3client, src_bucketname, dest_bucketname, prefix, size_limit):
-    src_bucket = s3.Bucket(src_bucketname)
-    dest_bucket = s3.Bucket(dest_bucketname)
-    for k in dest_bucket.objects.filter(Prefix=prefix):
-        fixup_object(s3client, k, src_bucketname, src_bucket, dest_bucketname, dest_bucket, size_limit)
-
-def fixup_object(s3client, k, src_bucketname, src_bucket, dest_bucketname, dest_bucket, size_limit):
-    if verbose: print('fixup_object', k.key)
-    o = dest_bucket.Object(k.key)
-    if 'user-md5' in o.metadata:
-        return
-    if 1: print('user-md5 missing in', dest_bucketname, k.key)
-    srco = src_bucket.Object(k.key)
-    if not 'user-md5' in srco.metadata:
-        if 1: print('+ user-md5 missing in', src_bucketname, k.key)
-
-        if size_limit is not None and o.content_length > size_limit:
-            print('skipping', k.key, o.content_length)
-            return
-            
-        # diff src and dest objects
-        srcf, srcfile = tempfile.mkstemp()
-        os.close(srcf)
-        print('download src', src_bucketname, k.key, srcfile)
-        srco.download_file(srcfile)
-
-        destf, destfile = tempfile.mkstemp()
-        os.close(destf)
-        print('download dest', dest_bucketname, k.key, destfile)
-        o.download_file(destfile)
-
-        if not file_cmp(srcfile, destfile):
-            print(k.key, 'file cmp failed', srcfile, destfile)
-            return
-
-        # compute md5 checksums
-        src_md5 = compute_md5(srcfile)
-        dest_md5 = compute_md5(destfile)
-        if src_md5 != dest_md5:
-            print('diff md5', k.key, srcfile, 'src md5=', src_md5, destfile, 'dest md5=', dest_md5)
-            return
-
-        os.unlink(srcfile)
-        os.unlink(destfile)
-            
-        # compare with e_tag
-        src_tag = srco.e_tag.strip('"')
-        dest_tag = o.e_tag.strip('"')
-        if src_md5 != src_tag:
-            print('note diff etag', k.key, 'src etag=', src_tag, 'src md5=', src_md5)
-            print('note diff etag', k.key, 'dest etag=', src_tag, 'dest md5=', dest_md5)
-            
-        # update dest object metadata with checksum
-        print('fixing', k.key, 'user-md5=', dest_md5)
-        new_metadata = srco.metadata.copy()
-        new_metadata.update(o.metadata)
-        new_metadata['user-md5'] = dest_md5
-        s3client.copy_object(Bucket=dest_bucketname, Key=k.key, CopySource=dest_bucketname + '/' + k.key, Metadata=new_metadata, MetadataDirective='REPLACE')
+def get_repo(name):
+    if name.startswith('s3://'):
+        name = name[5:]
+        f = name.split('/')
+        bucket_name = f.pop(0)
+        prefix = '/'.join(f)
+        return S3Repo(bucket_name, prefix)
     else:
-        print('fixing', k.key, 'src-md5=', srco.metadata['user-md5'])
-        new_metadata = srco.metadata.copy()
-        new_metadata.update(o.metadata)
-        s3client.copy_object(Bucket=dest_bucketname, Key=k.key, CopySource=dest_bucketname + '/' + k.key, Metadata=new_metadata, MetadataDirective='REPLACE')
+        return LocalRepo(name)
+
+def fixup_bucket(src, dest, size_limit):
+    if dest.get_type() != 'S':
+        return
+    for key in dest.get_keys():
+        md5 = dest.get_md5(key)
+        if md5 is None:
+            fixup_object(key, src, dest, size_limit)
+
+def fixup_object(key, src, dest, size_limit):
+    global verbose
+    if verbose: print('fixup_object', dest.get_full_name(key))
+
+    # verify src_size == dest_size
+    try:
+        src_size = src.get_size(key)
+    except:
+        print('ERROR cant get size', src.get_full_name(key), file=sys.stderr)
+        return
+    try:
+        dest_size = dest.get_size(key)
+    except:
+        print('ERROR cant get size', dest.get_full_name(key), file=sys.stderr)
+        return
+    if src_size != dest_size:
+        print('ERROR size diff', src.get_full_name(key), src_size, dest.get_full_name(key), dest_size, file=sys.stderr)
+        return
+
+    # filter large files
+    if size_limit is not None and src_size > size_limit:
+        print('ERROR size', src.get_full_name(key), src_size, file=sys.stderr)
+        return
+
+    # verify src md5 exists
+    try:
+        src_md5 = src.get_md5(key)
+    except:
+        src_md5 = None
+    if src_md5 is None:
+        # TODO download and compare
+        print('ERROR src md5 missing', src.get_full_name(key), file=sys.stderr)
+        return
+
+    # set dest md5 == src md5
+    dest.set_md5(key, src_md5)
 
 def file_cmp(afile, bfile):
     if verbose: print('real_file_cmp', afile, bfile)
